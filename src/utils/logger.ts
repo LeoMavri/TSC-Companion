072fb93977d01428e4dff5ef2d405f3693eb47af
Mavri <me@mavri.dev>
2024-04-18 18:57:03 +0300

    fix: logging and spy fetching for PDA

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,22 +1,60 @@
 import Settings from './local-storage.js';
 import { Constants } from '../constants.js';
 
+const pdaKey = '###PDA-APIKEY###';
+const isPda = pdaKey.includes('PDA-APIKEY') === false;
+
 export default class Logger {
   public static info(message: string, ...obj: any): void {
-    console.info(`%c[TSC Companion] ${message}`, `color: ${Constants.Colours.Info}`, ...obj);
+    let colorFirst = '%c';
+    let colorEnd = `color: ${Constants.Colours.Info}`;
+
+    if (isPda) {
+      obj = obj.map((o: any) => JSON.stringify(o));
+      colorFirst = '';
+      colorEnd = '';
+    }
+
+    console.info(`${colorFirst}[TSC Companion] ${message}`, colorEnd, ...obj);
   }
 
   public static warn(message: string, ...obj: any): void {
-    console.log(`%c[TSC Companion] ${message}`, `color: ${Constants.Colours.Warn}`, ...obj);
+    let colorFirst = '%c';
+    let colorEnd = `color: ${Constants.Colours.Warn}`;
+
+    if (isPda) {
+      obj = obj.map((o: any) => JSON.stringify(o));
+      colorFirst = '';
+      colorEnd = '';
+    }
+
+    console.log(`${colorFirst}[TSC Companion] ${message}`, colorEnd, ...obj);
   }
 
   public static error(message: string, ...obj: any): void {
-    console.error(`%c[TSC Companion] ${message}`, `color: ${Constants.Colours.Error}`, ...obj);
+    let colorFirst = '%c';
+    let colorEnd = `color: ${Constants.Colours.Error}`;
+
+    if (isPda) {
+      obj = obj.map((o: any) => JSON.stringify(o));
+      colorFirst = '';
+      colorEnd = '';
+    }
+    console.error(`${colorFirst}[TSC Companion] ${message}`, colorEnd, ...obj);
   }
 
   public static debug(message: string, ...obj: any): void {
     if (!Settings.getToggle('debug-logs')) return;
 
-    console.log(`%c[TSC Companion] ${message}`, `color: ${Constants.Colours.Debug}`, ...obj);
+    let colorFirst = '%c';
+    let colorEnd = `color: ${Constants.Colours.Debug}`;
+
+    if (isPda) {
+      obj = obj.map((o: any) => JSON.stringify(o));
+      colorFirst = '';
+      colorEnd = '';
+    }
+
+    console.log(`${colorFirst}[TSC Companion] ${message}`, colorEnd, ...obj);
   }
 }
