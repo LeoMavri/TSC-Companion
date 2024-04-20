c293d436dbdc2f81f3a068b198a88ca7ba453e69
Mavri <me@mavri.dev>
2024-04-20 15:05:15 +0300

    misc: added guard clause to xhook call

--- src/pages/faction/war/index.ts
+++ src/pages/faction/war/index.ts
@@ -11,6 +11,7 @@ import Page from '../../page.js';
 
 const ID_HREF_SELECTOR = 'a[href^="/profiles.php?XID="]';
 const INFO_BOX_SELECTOR = '[class*="userInfoBox"]';
+const MEMBER_SELECTOR = 'div.member.icons.left';
 
 export const FactionWar = new Page({
   name: 'Faction - War',
@@ -22,58 +23,59 @@ export const FactionWar = new Page({
 
   start: async function () {
     xhook.after(async (request, _response) => {
-      if (request.url.includes('faction_wars.php?redirect=false&step=getwarusers&')) {
-        const user = await waitForElement('div.member.icons.left', 10_000);
+      if (request.url.includes('faction_wars.php?redirect=false&step=getwarusers&') === false) {
+        return;
+      }
+      const user = await waitForElement(MEMBER_SELECTOR, 10_000);
 
-        if (!user) {
-          Logger.error(`${this.name}: Could not find users`);
+      if (!user) {
+        Logger.error(`${this.name}: Could not find users`);
+        return;
+      }
+
+      $(MEMBER_SELECTOR).each((_index, element) => {
+        Logger.debug('element', element);
+        const userHref = $(element).find<HTMLAnchorElement>(ID_HREF_SELECTOR)[0];
+
+        if (!userHref) {
+          Logger.error(`${this.name}: Could not find the user's Href`);
           return;
         }
 
-        $('div.member.icons.left').each((_index, element) => {
-          Logger.debug('element', element);
-          const userHref = $(element).find<HTMLAnchorElement>(ID_HREF_SELECTOR)[0];
+        const userId = userHref.href.split('XID=')[1];
 
-          if (!userHref) {
-            Logger.error(`${this.name}: Could not find the user's Href`);
+        if (!userId) {
+          Logger.error(`${this.name}: Could not find the user's ID`);
+          return;
+        }
+
+        const member = $(element).find<HTMLDivElement>(INFO_BOX_SELECTOR)[0];
+
+        if (!member) {
+          Logger.debug(`${this.name}: Could not find member's info box.`);
+          return;
+        }
+
+        getTSCSpyOld(userId).then(spy => {
+          if ('error' in spy || spy.success !== true) {
+            Logger.warn(`${this.name}: Failed to find spy for ${userId}`, spy);
             return;
           }
 
-          const userId = userHref.href.split('XID=')[1];
+          const { spyText, tooltipText } = formatSpy(spy);
 
-          if (!userId) {
-            Logger.error(`${this.name}: Could not find the user's ID`);
-            return;
+          const div = $('<div>').text(spyText).attr('title', tooltipText);
+
+          // todo: make this work on TT wars as well
+          if (window.location.href.includes('war/rank')) {
+            div.addClass('tsc-faction-rw');
+          } else {
+            div.addClass('tsc-faction-war');
           }
 
-          const member = $(element).find<HTMLDivElement>(INFO_BOX_SELECTOR)[0];
-
-          if (!member) {
-            Logger.debug(`${this.name}: Could not find member's info box.`);
-            return;
-          }
-
-          getTSCSpyOld(userId).then(spy => {
-            if ('error' in spy || spy.success !== true) {
-              Logger.warn(`${this.name}: Failed to find spy for ${userId}`, spy);
-              return;
-            }
-
-            const { spyText, tooltipText } = formatSpy(spy);
-
-            const div = $('<div>').text(spyText).attr('title', tooltipText);
-
-            // todo: make this work on TT wars as well
-            if (window.location.href.includes('war/rank')) {
-              div.addClass('tsc-faction-rw');
-            } else {
-              div.addClass('tsc-faction-war');
-            }
-
-            $(member).append(div);
-          });
+          $(member).append(div);
         });
-      }
+      });
     });
   },
 });
