6ea86da2c057010381831b4f891654ad19b7257e
Mavri <me@mavri.dev>
2024-05-05 23:06:09 +0300

    feat: war and chain show up instantly

--- src/pages/faction/chain/index.ts
+++ src/pages/faction/chain/index.ts
@@ -1,5 +1,7 @@
 import './faction-chain.css';
 
+import xhook from 'xhook';
+
 import { getTSCSpyOld } from '../../../utils/api.js';
 import { waitForElement } from '../../../utils/dom.js';
 import { formatSpy } from '../../../utils/format.js';
@@ -11,20 +13,49 @@ const CHAIN_ITEM_SELECTOR = '[class^="warListItem"][class*="first-in-row"]';
 const ATTACK_LIST_SELECTOR = '[class^="chain-attacks-list"]';
 const NAME_SELECTOR = '[class^="honorWrap"]';
 
+const addStats = async (): Promise<void> => {
+  $(`${ATTACK_LIST_SELECTOR} li`).each(function (_index: number, element: HTMLElement) {
+    const users = $(element).find(NAME_SELECTOR);
+
+    for (let i = 0; i <= 1; i++) {
+      const u = users[i];
+      const id = $(u).find('a').attr('href');
+
+      if (!id) {
+        Logger.warn('Faction - Chain: Failed to find ID.');
+        return;
+      }
+
+      const userId = id.split('XID=')[1];
+
+      getTSCSpyOld(userId).then(spy => {
+        if ('error' in spy || spy.success !== true) {
+          Logger.warn(`Faction - Chain: Failed to find spy for ${userId}`, spy);
+          return;
+        }
+
+        const { spyText, tooltipText } = formatSpy(spy);
+        // I know this technically wastes a call, but it seems it only works like this
+
+        if ($(u).find('.tsc-chain-spy').length > 0) {
+          return;
+        }
+
+        $(u).append($('<div>').addClass('tsc-chain-spy').text(spyText).attr('title', tooltipText));
+      });
+    }
+  });
+};
+
 export const FactionChain = new Page({
   name: 'Faction - Chain',
   description: 'Shows spies on the chain page',
-
-  /**
-   TODO: Generalise the selector in case the __eE_Ve part changes
-   TODO: Make it run if you visit the chain page directly (this only runs if you visit the faction page first)
-   */
 
   shouldRun: async function () {
     if (!Settings.getToggle(this.name) || !window.location.href.includes('factions.php?step=your'))
       return false;
 
-    const el = await waitForElement(CHAIN_ITEM_SELECTOR);
+    const el = await waitForElement(CHAIN_ITEM_SELECTOR, 15_000);
 
     return el !== null;
   },
@@ -32,77 +63,41 @@ export const FactionChain = new Page({
   start: async function () {
     let updateChainMO: MutationObserver | null = null;
 
-    const chainBig = document.querySelector(CHAIN_ITEM_SELECTOR);
-
-    const observer = new MutationObserver(async mutations => {
-      if (mutations.length === 0) return;
-
-      const newElement = document.querySelector(CHAIN_ITEM_SELECTOR);
-
-      if (!newElement?.classList.contains('act')) {
+    xhook.after(async (request, _response) => {
+      if (request.url.includes('warID=chain') === false) {
         return;
       }
 
-      Logger.debug(`${this.name}: Chain is active`);
+      Logger.debug(`${this.name}: Found Chain War users request:`, request.url);
+
+      const attacks = await waitForElement(ATTACK_LIST_SELECTOR, 15_000);
+
+      if (!attacks) {
+        Logger.error(`${this.name}: Could not find attacks list (element did not show up in time)`);
+        return;
+      }
+
       if (updateChainMO !== null) {
         updateChainMO.disconnect();
         updateChainMO = null;
       }
 
-      const attacks = await waitForElement(ATTACK_LIST_SELECTOR, 15_000);
-
-      if (!attacks) {
-        Logger.debug(`${this.name}: Could not find attacks list (element did not show up in time)`);
-        return;
-      }
-
-      const addAttacks = async (): Promise<void> => {
-        $(`${ATTACK_LIST_SELECTOR} li`).each(function (_index: number, element: HTMLElement) {
-          const users = $(element).find(NAME_SELECTOR);
-
-          for (let i = 0; i <= 1; i++) {
-            const u = users[i];
-
-            if ($(u).parent().find('.tsc-chain-spy').length > 0) {
-              continue;
-            }
-
-            const id = $(u).find('a').attr('href');
-
-            if (!id) {
-              Logger.warn('Faction - Chain: Failed to find ID.');
-              return;
-            }
-
-            const userId = id.split('XID=')[1];
-
-            getTSCSpyOld(userId).then(spy => {
-              if ('error' in spy || spy.success !== true) {
-                Logger.warn(`Faction - Chain: Failed to find spy for ${userId}`, spy);
-                return;
-              }
-
-              const { spyText, tooltipText } = formatSpy(spy);
-
-              $(u).append(
-                $('<div>').addClass('tsc-chain-spy').text(spyText).attr('title', tooltipText)
-              );
-            });
-          }
-        });
-      };
+      await addStats();
 
       updateChainMO = new MutationObserver(async _mutations => {
         let redo = false;
+
         $(`${CHAIN_ITEM_SELECTOR} li`).each(function (_index: number, element: HTMLElement) {
-          if ($(element).find('.tsc-chain-spy').length === 0 && redo === false) {
+          if (redo === true) {
+            return;
+          }
+
+          if ($(element).find('.tsc-chain-spy').length === 0) {
             redo = true;
-            addAttacks();
+            addStats();
           }
         });
       });
-
-      await addAttacks();
 
       updateChainMO.observe(attacks, {
         childList: true,
@@ -110,14 +105,87 @@ export const FactionChain = new Page({
       });
     });
 
-    if (!chainBig) {
-      Logger.error(`${this.name}: Could not find element`);
-      return;
-    }
+    // The chain is already opened
+    if (window.location.href.includes('war/chain')) {
+      const attacks = await waitForElement(ATTACK_LIST_SELECTOR, 15_000);
 
-    observer.observe(chainBig, {
-      attributes: true,
-      attributeFilter: ['class'],
-    });
+      if (!attacks) {
+        Logger.error(`${this.name}: Could not find attacks list (element did not show up in time)`);
+        return;
+      }
+
+      await addStats();
+    }
   },
 });
+
+// const chainBig = document.querySelector(CHAIN_ITEM_SELECTOR);
+// if (!chainBig) {
+//   Logger.error(`${this.name}: Could not find element`);
+//   return;
+// }
+// const observer = new MutationObserver(async mutations => {
+//   if (mutations.length === 0) return;
+//   const newElement = document.querySelector(CHAIN_ITEM_SELECTOR);
+//   if (!newElement?.classList.contains('act')) {
+//     return;
+//   }
+//   Logger.debug(`${this.name}: Chain is active`);
+//   if (updateChainMO !== null) {
+//     updateChainMO.disconnect();
+//     updateChainMO = null;
+//   }
+//   // RW: faction_wars.php?redirect=false&step=getwarusers&factionID=ID_HERE&userID=0&warID=rank&rfcv=somethingHere
+//   // CH: faction_wars.php?redirect=false&step=getwarusers&factionID=0&userID=0&warID=chain&rfcv=somethingHere
+//   const attacks = await waitForElement(ATTACK_LIST_SELECTOR, 15_000);
+//   if (!attacks) {
+//     Logger.debug(`${this.name}: Could not find attacks list (element did not show up in time)`);
+//     return;
+//   }
+// const addAttacks = async (): Promise<void> => {
+//   $(`${ATTACK_LIST_SELECTOR} li`).each(function (_index: number, element: HTMLElement) {
+//     const users = $(element).find(NAME_SELECTOR);
+//     for (let i = 0; i <= 1; i++) {
+//       const u = users[i];
+//       const id = $(u).find('a').attr('href');
+//       if (!id) {
+//         Logger.warn('Faction - Chain: Failed to find ID.');
+//         return;
+//       }
+//       const userId = id.split('XID=')[1];
+//       getTSCSpyOld(userId).then(spy => {
+//         if ('error' in spy || spy.success !== true) {
+//           Logger.warn(`Faction - Chain: Failed to find spy for ${userId}`, spy);
+//           return;
+//         }
+//         const { spyText, tooltipText } = formatSpy(spy);
+//         // I know this technically wastes a call, but it seems it only works like this
+//         if ($(u).find('.tsc-chain-spy').length > 0) {
+//           return;
+//         }
+//         $(u).append(
+//           $('<div>').addClass('tsc-chain-spy').text(spyText).attr('title', tooltipText)
+//         );
+//       });
+//     }
+//   });
+// };
+// updateChainMO = new MutationObserver(async _mutations => {
+//   let redo = false;
+//   $(`${CHAIN_ITEM_SELECTOR} li`).each(function (_index: number, element: HTMLElement) {
+//     if ($(element).find('.tsc-chain-spy').length === 0 && redo === false) {
+//       redo = true;
+//       addAttacks();
+//     }
+//   });
+// });
+// await addAttacks();
+// updateChainMO.observe(attacks, {
+//   childList: true,
+//   subtree: true,
+// });
+// });
+// observer.observe(chainBig, {
+//   attributes: true,
+//   attributeFilter: ['class'],
+// });
--- src/pages/faction/war/index.ts
+++ src/pages/faction/war/index.ts
@@ -13,6 +13,74 @@ const ID_HREF_SELECTOR = 'a[href^="/profiles.php?XID="]';
 const INFO_BOX_SELECTOR = '[class*="userInfoBox"]';
 const MEMBER_SELECTOR = 'div.member.icons.left';
 
+const addStats = async (name: string): Promise<void> => {
+  const user = await waitForElement(MEMBER_SELECTOR, 10_000);
+
+  if (!user) {
+    Logger.error(`${name}: Could not find users`);
+    return;
+  }
+
+  $(MEMBER_SELECTOR).each((_index, element) => {
+    const userHref = $(element).find<HTMLAnchorElement>(ID_HREF_SELECTOR)[0];
+
+    if (!userHref) {
+      Logger.error(`${name}: Could not find the user's Href`);
+      return;
+    }
+
+    const userId = userHref.href.split('XID=')[1];
+
+    if (!userId) {
+      Logger.error(`${name}: Could not find the user's ID`);
+      return;
+    }
+
+    const member = $(element).find<HTMLDivElement>(INFO_BOX_SELECTOR)[0];
+
+    if (!member) {
+      Logger.debug(`${name}: Could not find member's info box.`);
+      return;
+    }
+
+    // This isn't ideal, but it should *hopefully* get rid of the racing condition
+    if ($(element).parent().find('.tsc-faction-war').length > 0) {
+      return;
+    }
+
+    const parentDiv = $('<div>').addClass('tsc-faction-war');
+    parentDiv.append($('<img>').addClass('tsc-loader'));
+
+    $(element).parent().append(parentDiv);
+
+    getTSCSpyOld(userId).then(spy => {
+      parentDiv.empty();
+
+      if ('error' in spy || spy.success !== true) {
+        Logger.warn(`${name}: Failed to find spy for ${userId}`, spy);
+
+        if ('error' in spy) {
+          $(parentDiv).append($('<span>').text(spy.message));
+        } else {
+          $(parentDiv).append($('<span>').text(errorToString(spy.code)));
+        }
+
+        return;
+      }
+
+      const { longTextInterval, longTextEstimate, toolTipText } = formatSpyLong(spy);
+
+      parentDiv.attr('title', toolTipText).append($('<span>').text(longTextEstimate));
+
+      if (longTextInterval !== '') {
+        $(parentDiv).append($('<span>').text(longTextInterval));
+      }
+
+      $(element).parent().append(parentDiv);
+    });
+  });
+};
+
 export const FactionWar = new Page({
   name: 'Faction - War',
   description: 'Shows spies on the faction war page',
@@ -23,69 +91,17 @@ export const FactionWar = new Page({
 
   start: async function () {
     xhook.after(async (request, _response) => {
-      if (request.url.includes('faction_wars.php?redirect=false&step=getwarusers&') === false) {
-        return;
-      }
-      const user = await waitForElement(MEMBER_SELECTOR, 10_000);
-
-      if (!user) {
-        Logger.error(`${this.name}: Could not find users`);
+      if (request.url.includes('&warID=rank') === false) {
         return;
       }
 
-      $(MEMBER_SELECTOR).each((_index, element) => {
-        const userHref = $(element).find<HTMLAnchorElement>(ID_HREF_SELECTOR)[0];
+      Logger.debug(`${this.name}: Found Ranked War users request: ${request.url}`);
 
-        if (!userHref) {
-          Logger.error(`${this.name}: Could not find the user's Href`);
-          return;
-        }
-
-        const userId = userHref.href.split('XID=')[1];
-
-        if (!userId) {
-          Logger.error(`${this.name}: Could not find the user's ID`);
-          return;
-        }
-
-        const member = $(element).find<HTMLDivElement>(INFO_BOX_SELECTOR)[0];
-
-        if (!member) {
-          Logger.debug(`${this.name}: Could not find member's info box.`);
-          return;
-        }
-
-        const parentDiv = $('<div>').addClass('tsc-faction-war');
-        parentDiv.append($('<img>').addClass('tsc-loader'));
-
-        $(element).parent().append(parentDiv);
-
-        getTSCSpyOld(userId).then(spy => {
-          parentDiv.empty();
-
-          if ('error' in spy || spy.success !== true) {
-            Logger.warn(`${this.name}: Failed to find spy for ${userId}`, spy);
-
-            if ('error' in spy) {
-              $(parentDiv).append($('<span>').text(spy.message));
-            } else {
-              $(parentDiv).append($('<span>').text(errorToString(spy.code)));
-            }
-
-            return;
-          }
-
-          const { longTextInterval, longTextEstimate, toolTipText } = formatSpyLong(spy);
-
-          parentDiv.attr('title', toolTipText).append($('<span>').text(longTextEstimate));
-
-          if (longTextInterval !== '') {
-            $(parentDiv).append($('<span>').text(longTextInterval));
-          }
-
-          $(element).parent().append(parentDiv);
-        });
-      });
+      await addStats(this.name);
     });
+
+    if (window.location.href.includes('/war/rank')) {
+      await addStats(this.name);
+    }
   },
 });
