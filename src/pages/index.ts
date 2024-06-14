20aee026b2abb18221a6ddb935a52c59868852aa
titanic-5 <145130511+titanic-5@users.noreply.github.com>
2024-06-14 02:40:20 +0100

    changes for pmarket

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,5 @@ export { FactionNormal } from './faction/normal/index.js';
 export { FactionChain } from './faction/chain/index.js';
 export { CompanyPage } from './company/index.js';
 export { FactionWar } from './faction/war/index.js';
-export { AbroadPage } from './abroad/index.js';
+export { AbroadPage } from './abroad/index.js'
+export { pMarket } from './pmarket/index.js';
---
+++ src/pages/pmarket/index.ts
@@ -0,0 +1,63 @@
+import './pmarket.css';
+
+import { getTSCSpyOld } from '../../utils/api.js';
+import { waitForElement } from '../../utils/dom.js';
+import { formatSpy } from '../../utils/format.js';
+import Settings from '../../utils/local-storage.js';
+import Logger from '../../utils/logger.js';
+import Page from '../page.js';
+
+const PMARKET_MEMBERS_SELECTOR =
+  '#mainContainer > div.content-wrapper.spring > div.points-market > div.chart-main-wrap > ul:nth-of-type(2)';
+const ID_HREF_SELECTOR = 'a[href^="/profiles.php?XID="]';
+
+export const pMarket = new Page({
+  name: 'Points Market',
+  description: 'Shows user spies on the points market',
+
+  shouldRun: async function () {
+    return (
+      Settings.getToggle(this.name) &&
+      window.location.pathname === '/pmarket.php'
+    );
+  },
+
+  start: async function () {
+    const users = await waitForElement(PMARKET_MEMBERS_SELECTOR, 15_000);
+
+    if (users === null) {
+      Logger.warn(`${this.name}: Failed to find element to append to.`);
+      return;
+    }
+
+    Logger.debug(`${this.name}: Found users`, users);
+
+    $(users)
+      .children('li')
+      .each((_index, user) => {
+        const userAnchor = $(user).find<HTMLAnchorElement>(ID_HREF_SELECTOR)[0];
+
+        if (userAnchor === undefined) {
+          Logger.debug(`${this.name}: Failed to find user's ID`, user);
+          return;
+        }
+
+        const id = userAnchor.href.split('XID=')[1];
+
+        Logger.debug(`${this.name}: Found user ID`, id);
+
+        getTSCSpyOld(id).then((spy) => {
+          if ('error' in spy || spy.success !== true) {
+            Logger.warn(`${this.name}: Failed to find spy for ${id}`, spy);
+            return;
+          }
+
+          const { spyText, tooltipText } = formatSpy(spy);
+
+          $(userAnchor).after(
+            $('<div>').addClass('tsc-company-spy').text(spyText).attr('title', tooltipText)
+          );
+        });
+      });
+  },
+});
